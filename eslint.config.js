afc243fec7b3d338d59ca569155cf088b0d6d077
revdol <revdol@163.com>
2025-03-11 12:26:58 +0800

    set eslint prettier

--- eslint.config.js
+++ eslint.config.js
@@ -25,4 +25,19 @@ export default [
   js.configs.recommended,
   ...pluginVue.configs['flat/essential'],
   skipFormatting,
+  //=========== 自定义规则
+  {
+    rules: {
+      'vue/multi-word-component-names': [
+        'warn',
+        {
+          ignores: ['index'], // vue组件名称多单词组成（忽略index.vue）
+        },
+      ],
+      'vue/no-setup-props-destructure': ['off'], // 关闭 props 解构的校验
+
+      'no-undef': 'error',
+    },
+  },
+  //=========== 规则结束
 ]
